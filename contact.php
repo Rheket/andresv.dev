39bb63083831e8837da83eee45e02b3e5b33df39
Andres Vargas <andresv9911@gmail.com>
2021-10-15 22:00:17 -0700

    Fixed php contact form

--- contact.php
+++ contact.php
@@ -1,5 +1,25 @@
 <?php
 
     $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'rheket@gmail.com';
+
+    $email_subject = 'Portfolio site contact';
+
+    $email_body =  "User Name: $name.\n".
+                        "User Email: $email.\n".
+                        "User Message: $message.\n";
+
+    $to = "andresv9911@gmail.com";
+
+    $headers = "From" $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("location: index.html");
 
 ?>
